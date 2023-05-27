083641e73c2646f72c6305490d1e0adf1626cbcb
kaisarb <kaisarb@gmail.com>
2023-05-27 12:45:37 +0600

    Get fetch one record by ID added

--- src/main/java/com/kb/postgresql/controller/DepartmentController.java
+++ src/main/java/com/kb/postgresql/controller/DepartmentController.java
@@ -4,10 +4,7 @@ import com.kb.postgresql.entity.Department;
 import com.kb.postgresql.service.DepartmentService;
 import com.kb.postgresql.service.DepartmentServiceImpl;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -25,5 +22,10 @@ public class DepartmentController {
     @GetMapping("/departments")
     public List<Department> fetchDepartmentList() {
         return departmentService.fetchDepartmentList();
+    }
+
+    @GetMapping("/departments/{id}")
+    public Department fetchDepartmentById(@PathVariable("id") Long departmentId) {
+        return departmentService.fetchDepartmentById(departmentId);
     }
 }
